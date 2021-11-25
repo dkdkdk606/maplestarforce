da9401da251b2724198f2aea1d035bbcc3dde47d
bbc <dkdkdk606@nate.com>
2021-11-25 10:09:42 +0900

    뭔가 이상해서 다시 올림test가 원격에 제대로 안올라간듯

--- logic/sf_once.py
+++ logic/sf_once.py
@@ -2,14 +2,14 @@ import random
 
 from sf_table import *
 from sf_process import sf_success
-from sf_process import sf_failure
+#from sf_process import sf_failure
 
 def starforce(level, sf_now, fail, meso_now, sale, fifth, starcatch):
 # 다 전역 변수로 만듦
-
 # 어 내가 하려는게 필요한 수치 넣으면 강화 한번 해주는거
 # level 장비레벨,       sf_now 현스타포스 수치, (필요없네, sf_fin), fail찬스타임계산용,
 # meso 현재까지 쓴 매소,    sale 할인 이벤트,       fifth 5,10,15 성공 이벤트
+    
     meso_now = meso_now + meso(level, sf_now) * (1-0.01*sale)
 
 #세일, fifth 정의 필요
@@ -17,22 +17,24 @@ def starforce(level, sf_now, fail, meso_now, sale, fifth, starcatch):
     lucky = random.uniform(0, 100)
 
 # 성공은 여러가지 경우 이벤트, 2연속 실패 등 이 있어서 프로세스에 결과값까지 나올수 있게 함
+
     if lucky <= sf_success(sf_now, fail, fifth, starcatch):
         sf_now += 1
-        print(sf_now+100)
 
-
+    
 # 성공할만큼 운좋진 않고 파괴될만큼 운 안좋진 않다면
     elif lucky <= 100 - sf_dist(sf_now, starcatch):
-        sf_failure(sf_now, fail)
-        print(sf_now+1000)
+#        sf_failure(sf_now, fail) #함수 안에서 sf_now가 하나 낮아져서 나와야하는데 지역함수 취급이어서 안나오네 즉 밖에 빼야하네 ㅄ
+        if sf_now not in [0, 1, 2, 3, 4, 5, 10, 15, 20]: #각 강화에선 유지니까 내릴필요 없 
+            fail += 1
+            sf_now -= 1
 
     else:
         fail = 0
         sf_now = 12
-        print(sf_now+10000)
+
     
-    return
+    return [sf_now, fail, meso_now]
 
           # 성공도 실패도 아니다? 바로 파괴
           # 10까지는 안떨구고 파괴 이런것도 다 넣음 실패확률 0 으로 파괴는 ㄴㄴ
--- logic/sf_process.py
+++ logic/sf_process.py
@@ -4,19 +4,27 @@ from sf_table import sf_succ
 def sf_success(sf_now, fail, fifth, starcatch):
     if fail == 2:
         fail = 0
+
         return 100.0
     
     elif fifth == 1 and sf_now in [5,10,15]:
+
         return 100.0
 
     else:
+
         return sf_succ(sf_now, starcatch)
     
-def sf_failure(sf_now, fail):
+def sf_failure(sf_now, fail): 
+        
 
     if sf_now not in [0, 1, 2, 3, 4, 5, 10, 15, 20]:
+       
         fail += 1
         sf_now -= 1
+        #이런거 넣으면 안되네 밖에다가 빼서 해야지 하는방법 있긴 할라나
+
     return
+
 #왜 fail 참조가 안될까??????????????????????? 엄란어라ㅣㅁㄴ어라ㅓ나ㅣㅏ
     
